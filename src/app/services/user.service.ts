bac45707d99b7cc8da7bbba1268de6d956d8fd94
MarceloTM-2001 <62965036+MarceloTM-2001@users.noreply.github.com>
2022-10-26 00:42:37 -0600

    User service added
    Servicio para todas las rutas con info del usuario loggeado

---
+++ src/app/services/user.service.ts
@@ -0,0 +1,40 @@
+import { TmplAstRecursiveVisitor } from '@angular/compiler';
+import { Injectable, OnInit } from '@angular/core';
+import { ClientsListI } from '../models/clientslist.interface';
+
+
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class UserService{
+   private userData: string[];
+   private usertype:boolean;
+  
+  constructor() { 
+    this.userData= ['Marcos','Gonzalez','118320994','60583801','algo@gmail.com','Calle patito del valle','Cartago','Costa Rica','miusuario','123456789'],
+    this.usertype= true //true cliente | false taller
+    console.log("It should be initialized2")
+  }
+
+  getType(){
+    return this.usertype
+  }
+  getData() {
+    return this.userData
+  };
+
+  setType(type:boolean){
+    this.usertype=type
+  };
+
+  setUser(users:ClientsListI[]){
+    this.userData = []
+    for (var user of users) {
+      this.userData.push(user.FirstN,user.LastN,user.ID,user.PhoneNum,user.Email,user.Address,user.City,user.Country,user.Username,user.Password) 
+    }
+  };
+
+
+}
