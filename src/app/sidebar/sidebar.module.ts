151cb09b965244f105a192ed89979393b2d273ab
MarceloTM-2001 <62965036+MarceloTM-2001@users.noreply.github.com>
2022-10-26 00:43:53 -0600

    Servicio en el module sidebar

--- src/app/sidebar/sidebar.module.ts
+++ src/app/sidebar/sidebar.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { SidebarComponent } from './sidebar.component';
+import { UserService } from 'app/services/user.service';
 
 @NgModule({
     imports: [ RouterModule, CommonModule ],
-    declarations: [ SidebarComponent ],
-    exports: [ SidebarComponent ]
+    declarations: [ SidebarComponent],
+    providers: [UserService],
+    exports: [SidebarComponent]
+    
 })
 
 export class SidebarModule {}
