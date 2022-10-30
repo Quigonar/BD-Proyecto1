a158426fb9b7b06bde48e860f9f780ff8890d3b9
MarceloTM-2001 <62965036+MarceloTM-2001@users.noreply.github.com>
2022-10-30 02:37:20 -0600

    Accept

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -10,24 +10,38 @@ import { ClientsListI } from '../models/clientslist.interface';
 })
 
 export class UserService{
+<<<<<<< HEAD
    private userData: string[];
    private usertype:boolean;
    private id:number;
 
+=======
+   public userData: string[];
+   public id: string;
+   public usertype:boolean;
+>>>>>>> 47f06006fa939894438a39225ea0d9bdbdd6238f
   
   constructor() { 
+    this.id = ""
     this.userData= ['Marcos','Gonzalez','118320994','60583801','algo@gmail.com','Calle patito del valle','Cartago','Costa Rica','miusuario','123456789'],
     this.usertype= true //true cliente | false taller
     this.id=0
   }
 
+<<<<<<< HEAD
 
   getType(){
+=======
+  getType() {
+>>>>>>> 47f06006fa939894438a39225ea0d9bdbdd6238f
     return this.usertype
   }
   getData() {
     return this.userData
   };
+  getID() {
+    return this.id
+  }
 
   getID(){
     return this.id
@@ -36,12 +50,14 @@ export class UserService{
     this.usertype=type
   };
 
-  setUser(users:ClientsListI[]){
+  setUser(user:ClientsListI){
     this.userData = []
-    for (var user of users) {
-      this.userData.push(user.FirstN,user.LastN,user.ID,user.PhoneNum,user.Email,user.Address,user.City,user.Country,user.Username,user.Password) 
-    }
+    this.userData.push(user.FirstN,user.FirstLN,user.SecondLN,user.ID,user.PhoneNum.toString(),user.Email,user.Address.toString(),user.Username,user.Password,user.Points)
   };
+
+  setID(ID:string) {
+    this.id = ID
+  }
 
 
 }
